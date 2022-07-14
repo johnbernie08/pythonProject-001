44882503573ae611cdb5145a4f822f53b493df7d
jramir77 <johnbernie_ramirez@yahoo.com>
2022-07-15 03:34:45 +0800

    testing new function

--- main.py
+++ main.py
@@ -5,12 +5,12 @@
 
 from now import testing
 
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_function(a, b):
+    return a+ b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -20,5 +20,7 @@ if __name__ == '__main__':
     print("How are you")
     print("Good Morning")
     print(testing)
+
+    print(my_function(2, 4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
